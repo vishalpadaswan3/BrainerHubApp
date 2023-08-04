2360f0fff0c5a69544ebe09587c4d625a23eec40
Vishal Padaswan <vishalpadaswan@gmail.com>
2023-08-05 00:32:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,13 @@ from flask_marshmallow import Marshmallow
 import os
 import pandas as pd
 # Initialize app
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://if0_34754069:LW1Z7ltJS5yAtm@sqlXXX.infinityfree.com/if0_34754069_XXX'
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 # Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:vishal@localhost/brainer'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://if0_34754069:LW1Z7ltJS5yAtm@sqlXXX.infinityfree.com/if0_34754069_XXX'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
